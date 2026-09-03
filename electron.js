d1ce1b43044abbed73c3ab6cfec308f88068a409
qzizop-ctrl <qzizop@gmail.com>
2026-09-03 15:41:05 +0300

    Add files via upload

---
+++ electron.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 820,
+    minWidth: 900,
+    minHeight: 600,
+    icon: path.join(__dirname, 'build', 'icon.ico'),
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // Remove the default menu bar (File/Edit/View...) for a cleaner app look
+  Menu.setApplicationMenu(null);
+
+  // Load the built web app (output of "npm run build")
+  mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
