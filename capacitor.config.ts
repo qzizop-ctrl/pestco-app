d6a0d6eaefef48a6c615b3d72effd31ce7e21b88
qzizop-ctrl <qzizop@gmail.com>
2026-09-13 20:53:27 +0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.pestco.app",
-  appName: "Pest.Co",
+  appName: "PEST",
   webDir: "dist",
   server: {
     androidScheme: "https",
